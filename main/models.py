9f2f8e578206020791b9faed9326d5cc6e79362a
demono10000 <48636182+demono10000@users.noreply.github.com>
2022-11-12 13:27:02 +0100

    Fixed placing orders

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Service(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     sampleURL = models.URLField(max_length=200)
 
